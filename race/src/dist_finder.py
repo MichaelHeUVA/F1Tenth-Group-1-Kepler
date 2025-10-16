984ae46dbb6ec6d3f14de4a09ffdb244297b1951
Amogh Ghadge <amoghghadge22@gmail.com>
2025-10-16 14:36:23 -0400

    package init + dist_finder implemented

---
+++ race/src/dist_finder.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import rospy
+import math
+from sensor_msgs.msg import LaserScan
+from race.msg import pid_input
+
+# Some useful variable declarations.
+angle_range = 240	# Hokuyo 4LX has 240 degrees FoV for scan
+forward_projection = 1.5	# distance (in m) that we project the car forward for correcting the error. You have to adjust this.
+desired_distance = 0.9	# distance from the wall (in m). (defaults to right wall). You need to change this for the track
+vel = 15 		# this vel variable is not really used here.
+error = 0.0		# initialize the error
+car_length = 0.50 # Traxxas Rally is 20 inches or 0.5 meters. Useful variable.
+
+# Handle to the publisher that will publish on the error topic, messages of the type 'pid_input'
+pub = rospy.Publisher('error', pid_input, queue_size=10)
+
+
+def getRange(data,angle):
+	# data: single message from topic /scan
+    # angle: between -30 to 210 degrees, where 0 degrees is directly to the right, and 90 degrees is directly in front
+    # Outputs length in meters to object with angle in lidar scan field of view
+    # Make sure to take care of NaNs etc.
+    # DONE: implement
+
+	# take input angle and subtract angle_min and divide by angle_increment
+	angle_radians = math.radians(angle)
+	rospy.loginfo("Angle: %f", angle)
+	rospy.loginfo("Angle radians: %f", angle_radians)
+	rospy.loginfo("Angle min: %f", data.angle_min)
+	rospy.loginfo("Angle increment: %f", data.angle_increment)
+	
+	index = int((angle_radians - data.angle_min) / data.angle_increment)
+
+	if index >= 0 and index < len(data.ranges):
+		value = data.ranges[index]
+		if not math.isinf(value) and not math.isnan(value):
+			return value
+		
+	return None
+
+
+
+def callback(data):
+	global forward_projection
+
+	theta = 50 # you need to try different values for theta
+	a = getRange(data,theta) # obtain the ray distance for theta
+	b = getRange(data,0)	# obtain the ray distance for 0 degrees (i.e. directly to the right of the car)
+
+	if not a or not b:
+		rospy.loginfo("Invalid LIDAR readings a: %s, b: %s", a, b)
+		return
+
+	swing = math.radians(theta)
+
+	## Your code goes here to determine the projected error as per the alrorithm
+	# Compute Alpha, AB, and CD..and finally the error.
+	# DONE: implement
+	alpha = math.atan((a*math.cos(swing) - b) / (a * math.sin(swing)))
+	AB = b * math.cos(alpha)
+	CD = AB + forward_projection * math.sin(alpha)
+	error = desired_distance - CD
+	rospy.loginfo("Error: %f", error)
+
+	msg = pid_input()	# An empty msg is created of the type pid_input
+	# this is the error that you want to send to the PID for steering correction.
+	msg.pid_error = error
+	msg.pid_vel = vel		# velocity error can also be sent.
+	pub.publish(msg)
+
+
+if __name__ == '__main__':
+	print("Hokuyo LIDAR node started")
+	rospy.init_node('dist_finder',anonymous = True)
+	# DONE: Make sure you are subscribing to the correct car_x/scan topic on your racecar
+	rospy.Subscriber("/car_1/scan",LaserScan,callback)
+	rospy.spin()
