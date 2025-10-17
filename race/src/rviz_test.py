1a426708f838b168106ebcf8fa8056079237e40e
Bryan Katuari <bryankatuari@gmail.com>
2025-10-17 16:01:46 -0400

    rviz test

--- race/src/rviz_test.py
+++ race/src/rviz_test.py
@@ -10,13 +10,13 @@ from visualization_msgs.msg import Marker
 
 from tf.transformations import quaternion_from_euler
 
-sphere_marker_pub = rospy.Publisher("/sphere_marker", Marker, queue_size = 2)
-arrow_marker_pub = rospy.Publisher("/arrow_marker", Marker, queue_size = 2)
+sphere_marker_pub = rospy.Publisher("/sphere_marker", Marker, queue_size=2)
+arrow_marker_pub = rospy.Publisher("/arrow_marker", Marker, queue_size=2)
 prev_range = 0.0
 
 
 def callback(data):
-    
+
     global prev_range
     # pick out the middle range value (at index 540 - just an example)
     center = data.ranges[540]
@@ -24,11 +24,10 @@ def callback(data):
     # If NaN, pipe though the previous non NaN value
     if math.isnan(center):
         center = prev_range
-    
+
     prev_range = center
     mid = "Middle Range %s" % (center)
     rospy.loginfo(mid)
-
 
     # SPHERE MARKER
 
@@ -62,12 +61,12 @@ def callback(data):
     sphere_marker.pose.orientation.w = 1.0
 
     # Convert steering Euler angle (yaw) to quaternion
-    #example_steering = 10
-    #quaternion = quaternion_from_euler(0,0,example_steering) # args: Roll, Pitch, Yaw
-    #sphere_marker.pose.orientation.x = quaternion[0]
-    #sphere_marker.pose.orientation.y = quaternion[1]
-    #sphere_marker.pose.orientation.z = quaternion[2]
-    #sphere_marker.pose.orientation.w = quaternion[3]
+    # example_steering = 10
+    # quaternion = quaternion_from_euler(0,0,example_steering) # args: Roll, Pitch, Yaw
+    # sphere_marker.pose.orientation.x = quaternion[0]
+    # sphere_marker.pose.orientation.y = quaternion[1]
+    # sphere_marker.pose.orientation.z = quaternion[2]
+    # sphere_marker.pose.orientation.w = quaternion[3]
 
     sphere_marker_pub.publish(sphere_marker)
 
@@ -92,9 +91,8 @@ def callback(data):
 
     arrow_marker_pub.publish(arrow_marker)
 
-   
-if __name__=='__main__':
-    rospy.init_node("rviz_test", anonymous=False)   
-    sub = rospy.Subscriber("/car_9/scan",LaserScan, callback)
-    rospy.spin()
 
+if __name__ == "__main__":
+    rospy.init_node("rviz_test", anonymous=False)
+    sub = rospy.Subscriber("/car_9/scan", LaserScan, callback)
+    rospy.spin()
