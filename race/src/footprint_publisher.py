ad8cdf84246dbe1c7c2a163308f8be28ebe6f6ea
Bryan Katuari <bryankatuari@gmail.com>
2025-10-18 13:55:17 -0400

    rviz update

--- race/src/footprint_publisher.py
+++ race/src/footprint_publisher.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import PolygonStamped, Point32
 
 
-def make_rect_polygon(length, width):
-    L = length / 2.0
-    W = width / 2.0
+def make_rect_polygon(length_m, width_m):
+    L = length_m / 2.0
+    W = width_m / 2.0
+    # CCW rectangle around the origin (base_link)
     return [
         Point32(+L, +W, 0.0),
         Point32(+L, -W, 0.0),
@@ -14,20 +15,28 @@ def make_rect_polygon(length, width):
     ]
 
 
-if __name__ == "__main__":
+def main():
     rospy.init_node("footprint_publisher")
-    pub = rospy.Publisher("/viz/footprint", PolygonStamped, queue_size=1, latch=True)
 
-    length = rospy.get_param("~length", 0.52)
-    width = rospy.get_param("~width", 0.30)
-    frame_id = rospy.get_param("~frame_id", "base_link")
+    # ---- params (feel free to tweak) ----
+    frame_id = rospy.get_param("~frame_id", "car_1/base_link")
+    topic_out = rospy.get_param("~topic_out", "/car_1/footprint")
+    length_m = float(rospy.get_param("~length", 0.52))  # car length (m)
+    width_m = float(rospy.get_param("~width", 0.30))  # car width  (m)
+    hz = float(rospy.get_param("~rate", 5.0))  # publish rate
+
+    pub = rospy.Publisher(topic_out, PolygonStamped, queue_size=1)
 
     poly = PolygonStamped()
     poly.header.frame_id = frame_id
-    poly.polygon.points = make_rect_polygon(length, width)
+    poly.polygon.points = make_rect_polygon(length_m, width_m)
 
-    rate = rospy.Rate(2)
+    rate = rospy.Rate(hz)
     while not rospy.is_shutdown():
         poly.header.stamp = rospy.Time.now()
         pub.publish(poly)
         rate.sleep()
+
+
+if __name__ == "__main__":
+    main()
